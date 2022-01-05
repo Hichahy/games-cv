09e87523ae139445f0dc890a196b638161849115
Hichahy <75397284+Hichahy@users.noreply.github.com>
2022-01-05 13:10:00 +0100

    presence game

---
+++ src/components/gameCart/GameCart.tsx
@@ -0,0 +1,155 @@
+import './gameCart.scss'
+import React, { useEffect, useState } from 'react'
+import {
+  Container,
+  Spinner,
+  Badge,
+  Breadcrumb,
+  Nav,
+  Carousel,
+  Button
+} from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+import { IGames } from '../../types/types'
+import { LinkContainer } from 'react-router-bootstrap'
+
+interface IProps {
+  games: IGames[];
+  mobileMode: boolean;
+  loadGames: () => void;
+}
+
+const GameCart = ({ games, mobileMode, loadGames }: IProps) => {
+  const [showScreen, setShowScreen] = useState(false)
+
+  useEffect(() => {
+    loadGames()
+  }, [loadGames])
+
+  useEffect(() => {
+    if (mobileMode) {
+      setShowScreen(true)
+    } else {
+      setShowScreen(false)
+    }
+  }, [mobileMode])
+
+  const { id } = useParams()
+
+  const find = () => {
+    return games.find((x) => x.id === id)
+  }
+
+  const game = find()
+
+  const showScreenHandler = () => {
+    setShowScreen((prev) => !prev)
+  }
+
+  if (games.length < 1) {
+    return (
+      <div className="loading-box">
+        <Spinner
+          animation="border"
+          style={{ color: 'white', height: '7rem', width: '7rem' }}
+          role="status"
+        />
+        <span style={{ color: 'white' }}>Loading</span>
+      </div>
+    )
+  }
+
+  return (
+    <Container fluid className="container-cart">
+      <Breadcrumb className="breadcrumb">
+        <LinkContainer to="/games">
+          <Nav.Link>Gry</Nav.Link>
+        </LinkContainer>
+        <span style={{ color: 'white' }}>/</span>
+        <Breadcrumb.Item style={{ marginLeft: '10px' }} active>
+          {game?.name}
+        </Breadcrumb.Item>
+      </Breadcrumb>
+      <section className="cart-section1">
+        <div className="cart-box1">
+          <img className="image-cart" src={game?.image} alt="Card image" />
+        </div>
+        <div className="cart-box2">
+          <h1>{game?.name}</h1>
+          <p>{game?.description}</p>
+          <div>
+            <h4>Platforma</h4>
+            <div>
+              {game?.platform.includes('PlayStation 4')
+                ? (
+                <i className="bi bi-playstation"></i>
+                  )
+                : null}
+              {game?.platform.includes('xbox')
+                ? (
+                <i className="bi bi-xbox"></i>
+                  )
+                : null}
+              {game?.platform.includes('windows')
+                ? (
+                <i className="bi bi-windows"></i>
+                  )
+                : null}
+            </div>
+          </div>
+          <div>
+            <h4>Gatunek</h4>
+            <Badge pill bg="warning" text="dark">
+              {game?.type}
+            </Badge>
+          </div>
+        </div>
+      </section>
+      <section className="screen-pc">
+        <img
+          onClick={showScreenHandler}
+          src={game?.screenShot1}
+          alt="First slide"
+        />
+        <img onClick={showScreenHandler} src={game?.screenShot2} alt="Second slide" />
+        <img onClick={showScreenHandler} src={game?.screenShot3} alt="Third slide" />
+      </section>
+      {showScreen
+        ? (
+        <>
+          <Carousel className="show-slider">
+            <Carousel.Item>
+              <img
+                 src={game?.screenShot1}
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+              src={game?.screenShot2}
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                src={game?.screenShot3}
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+          <div className="overlay" onClick={showScreenHandler}></div>
+        </>
+          )
+        : null}
+        <section className="reckoning">
+        <div>
+        <i className="bi bi-cash-coin"></i>
+        <span>{game?.price} ¥ per day</span>
+        </div>
+        <Button variant="warning">rezerve now!</Button>{' '}
+        </section>
+    </Container>
+  )
+}
+
+export default GameCart
--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -1,6 +1,7 @@
 import './games.scss'
 import React, { useEffect } from 'react'
 import { Container, Row, Card, Spinner } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import { IGames } from '../../types/types'
 
 interface IProps {
@@ -17,8 +18,12 @@ const Games = ({ games, loadGames }: IProps) => {
   if (games.length < 1) {
     return (
       <div className="loading-box">
-        <Spinner animation="border" style={{ height: '7rem', width: '7rem' }} role="status" />
-        <span>Loading</span>
+        <Spinner
+          animation="border"
+          style={{ color: 'white', height: '7rem', width: '7rem' }}
+          role="status"
+        />
+        <span style={{ color: 'white' }}>Loading</span>
       </div>
     )
   }
@@ -26,17 +31,17 @@ const Games = ({ games, loadGames }: IProps) => {
   return (
     <Container fluid className="container-game">
       <Row className="game-box">
-        {games.map(
-          (game) => (
-            <Card key={game.id} className="cart-game">
+        {games.map((game) => (
+          <Card key={game.id} className="cart-game">
+            <Link to={`/game/${game.id}`}>
               <Card.Img className="img-game" variant="top" src={game.image} />
-              <Card.Body>
-                <Card.Title>{game.name}</Card.Title>
-                <Card.Text>{game.description}</Card.Text>
-              </Card.Body>
-            </Card>
-          )
-        )}
+            </Link>
+            <Card.Body>
+              <Card.Title>{game.name}</Card.Title>
+              <Card.Text>{game.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
       </Row>
     </Container>
   )
